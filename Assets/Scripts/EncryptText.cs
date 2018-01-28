1b2877b69462a05f47fbb6c45b6ca72d33a6900b
BeyondMeta <sophie.twardus@gmail.com>
2018-01-27 23:14:14 -0400

    Initial commit

---
+++ Assets/Scripts/EncryptText.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+public class EncryptText : MonoBehaviour {
+
+    public Text encryptedText;
+    public Text decryptedText;
+    public InputField inputField;
+
+
+    string myString;
+    const string glyphs = "abcdefghijklmnopqrstuvwxyz0123456789";
+    int j = 0;
+
+    void Start()
+    {
+        TextAsset textFile = (TextAsset)Resources.Load("storystring");
+        string story = textFile.ToString();
+
+        string[] storyArray = story.Split("\n"[0]);
+
+    }
+
+    void generateText(string[] sA)
+    {
+       
+            while (j < sA.Length)
+        {
+            int charAmount = sA[j].Length;
+            for (int i = 0; i < charAmount; i++)
+            {
+                myString += glyphs[UnityEngine.Random.Range(0, glyphs.Length)];
+            }
+            encryptedText.text = myString; 
+        }
+    }
+ 
+int charAmount = storyArray[j]; 
+    int 
+ for(int i=0; i<charAmount; i++)
+ {
+     myString += glyphs[Random.Range(0, glyphs.Length)];
+ }
+
+// Update is called once per frame
+void Update ()
+    {
+		if(j <= storyArray.Length)
+    {
+        CanvasManager.NextScreen("Conclusion");
+    }
+
+	}
+
+}
---
+++ Assets/Scripts/MenuControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuControl : MonoBehaviour
+{
+    public InputField usernameInput;
+    public static string username;
+
+    void Start()
+    {
+        if (username != null)
+            usernameInput.text = username;
+    }
+
+    public void SaveUsername(string newName)
+    {
+        username = newName;
+    }
+}
